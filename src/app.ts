113b52c2736ceb978cbbc0df6b8c9a5af50ff7f0
Annwar Triana <annwartriana@gmail.com>
2024-02-05 18:25:55 -0500

    Edit app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import { envs } from "./config/envs";
 import { Server } from "./presentation/server";
 
 (async()=>{
@@ -8,7 +9,7 @@ function main(){
     const server = new Server({
         port: envs.PORT,
         public_path: envs.PUBLIC_PATH,
-    };
+    });
     server.start();
     
 }
