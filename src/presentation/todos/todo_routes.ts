cd2d262146b9516e55feeff1acde6a5880f2157e
Annwar Triana <20819350+annwartriana@users.noreply.github.com>
2025-02-09 14:50:48 -0500

    Clean Architecture Implemented

--- src/presentation/todos/todo_routes.ts
+++ src/presentation/todos/todo_routes.ts
@@ -1,12 +1,18 @@
 import { Router } from "express";
 import { TodosController } from "./controller";
+import { TodoDatasourceImpl } from "../../infrastructure/datasources/todo.datasource.impl";
+import { TodoRepository } from "../../domain";
+import { TodoRepositoryImpl } from "../../infrastructure/repositories/todo.repository.impl";
 
 
 export class TodoRoutes {
     
     static get routes(): Router{
         const router = Router();
-        const todoController= new TodosController();
+
+        const dataSourceImpl = new TodoDatasourceImpl();
+        const todoRepository = new TodoRepositoryImpl(dataSourceImpl);
+        const todoController= new TodosController(todoRepository);
 
         router.get('/',  todoController.getTodos);
         router.get('/:id',  todoController.getTodoById );
